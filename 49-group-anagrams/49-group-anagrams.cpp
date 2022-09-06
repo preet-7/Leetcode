57450f5371559b99317f42daf36b4d8cf860525f
Preet <99633018+preet-7@users.noreply.github.com>
2022-09-06 13:02:28 +0530

    Time: 34 ms (95.15%), Space: 19.6 MB (83.66%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-		if(strs.size() == 1)
-            return {{strs[0]}};
+		if(strs.size() == 1) return {{strs[0]}};
         
         vector<vector<string>> ans;
         unordered_map<string, vector<string>> M;
