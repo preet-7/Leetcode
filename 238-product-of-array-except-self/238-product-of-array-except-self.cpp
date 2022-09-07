a5a8b8bb855b177e262f03ab4850c1f1faa68c4b
Preet <99633018+preet-7@users.noreply.github.com>
2022-09-07 11:49:34 +0530

    Time: 35 ms (65.52%), Space: 24.1 MB (67.81%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -7,16 +7,12 @@ public:
         for(int i=0;i<n;i++){
             result[i]=prefix;           
             prefix=prefix*nums[i];
-
         }
         int postfix=1;
         for(int i=n-1;i>=0;i--){
             result[i]=result[i]*postfix;           
             postfix=postfix*nums[i];
-
         }
-        return result; 
-        
-        
+        return result;        
     }
 };
