45b8898f184521233d7132b2046bd1573632b36a
Preet <99633018+preet-7@users.noreply.github.com>
2022-09-15 15:29:51 +0530

    Time: 16 ms (70.66%), Space: 21.8 MB (30.68%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -12,23 +12,26 @@
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
-        stack<TreeNode*> stk;
         TreeNode* prev = NULL;
+        return inorder(root, prev);
+    }
+private:
+    bool inorder(TreeNode* root, TreeNode*& prev) {
+        if (root == NULL) {
+            return true;
+        }
         
-        while (!stk.empty() || root != NULL) {
-            while (root != NULL) {
-                stk.push(root);
-                root = root->left;
-            }
-            root = stk.top();
-            stk.pop();
-            
-            if (prev != NULL && prev->val >= root->val) {
-                return false;
-            }
-            
-            prev = root;
-            root = root->right;
+        if (!inorder(root->left, prev)) {
+            return false;
+        }
+        
+        if (prev != NULL && prev->val >= root->val) {
+            return false;
+        }
+        prev = root;
+        
+        if (!inorder(root->right, prev)) {
+            return false;
         }
         
         return true;
